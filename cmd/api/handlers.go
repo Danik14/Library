eca91fdfb2581fe85b3af110953660260e7f126a
Danik <chapagana@gmail.com>
2022-12-07 16:02:08 +0600

    Added createBook

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Danik14/library/internal/models"
@@ -10,5 +11,34 @@ func (app *application) listAllBooks(w http.ResponseWriter, r *http.Request) {
 	book := models.NewBook(1, "AlibaSlave", "Danik", "1964", 300, []string{"gachi"}, 1)
 	env := envelope{"book": book}
 
-	app.writeJSON(w, http.StatusOK, env, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, "JSON error")
+		return
+	}
+}
+
+func (app *application) listOneBook(w http.ResponseWriter, r *http.Request) {
+	book := models.NewBook(1, "AlibaSlave", "Danik", "1964", 300, []string{"gachi"}, 1)
+
+	env := envelope{"book": book}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, "JSON error")
+		return
+	}
+}
+
+func (app *application) createBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+
+	err := app.readJSON(w, r, book)
+	if err != nil {
+		app.logError(r, err)
+		app.errorResponse(w, r, http.StatusBadRequest, "JSON error")
+		return
+	}
+
+	fmt.Println(book)
 }
