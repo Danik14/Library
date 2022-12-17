eb38156c2e7523f587664b4edb97ac139259d675
Danik <chapagana@gmail.com>
2022-12-17 12:57:15 +0600

    Added UUID type for books id

--- migrations/000003_create_books_table.up.sql
+++ migrations/000003_create_books_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS books (
-id bigserial PRIMARY KEY,
+id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
 created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
 title text NOT NULL,
 author text NOT NULL,
