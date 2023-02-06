266698d5b359dac5d3aee63f279b9114ecf587f2
alibekkenny <alibek.keneskhanov@gmail.com>
2023-02-07 00:37:30 +0600

    refactored code, deleted excess

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -93,24 +93,9 @@ func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request
 		app.serverErrorResponse(w, r, anyerr)
 	}
 
-	// // Call the Insert() method on our books model, passing in a pointer to the
-	// // validated book struct. This will create a record in the database and update the
-	// // book struct with the system-generated information.
-	// err = app.models.Books.Insert(book)
-	// if err != nil {
-	// 	app.serverErrorResponse(w, r, err)
-	// 	return
-	// }
-
-	// When sending a HTTP response, we want to include a Location header to let the
-	// client know which URL they can find the newly-created resource at. We make an
-	// empty http.Header map and then use the Set() method to add a new Location header,
-	// interpolating the system-generated ID for our new movie in the URL.
 	headers := make(http.Header)
 	headers.Set("Location", fmt.Sprintf("/v1/books/%d", book.ID))
 
-	// Write a JSON response with a 201 Created status code, the movie data in the
-	// response body, and the Location header.
 	err = app.writeJSON(w, http.StatusCreated, envelope{"book": book}, headers)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
--- internal/models/book.go
+++ internal/models/book.go
@@ -22,6 +22,18 @@ type BookModel struct {
 	DB *mongo.Collection
 }
 
+func ValidateBook(v *validator.Validator, book *Book) {
+
+	v.Check(book.Title != "", "title", "must be provided")
+	v.Check(len(book.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(book.Author != "", "author", "must be provided")
+	v.Check(len(book.Author) <= 500, "author", "must not be more than 500 bytes long")
+
+	v.Check(book.Year > 0, "year", "must be more than 0")
+	v.Check(book.Pages > 0, "pages", "must be more than 0")
+}
+
 // func (b BookModel) Insert(book *Book) error {
 
 // }
@@ -188,19 +200,3 @@ type BookModel struct {
 // 	}
 // 	return nil
 // }
-
-func ValidateBook(v *validator.Validator, book *Book) {
-	// Use the Check() method to execute our validation checks. This will add the
-	// provided key and error message to the errors map if the check does not evaluate
-	// to true. For example, in the first line here we "check that the title is not
-	// equal to the empty string". In the second, we "check that the length of the title
-	// is less than or equal to 500 bytes" and so on.
-	v.Check(book.Title != "", "title", "must be provided")
-	v.Check(len(book.Title) <= 500, "title", "must not be more than 500 bytes long")
-
-	v.Check(book.Author != "", "author", "must be provided")
-	v.Check(len(book.Author) <= 500, "author", "must not be more than 500 bytes long")
-
-	v.Check(book.Year > 0, "year", "must be more than 0")
-	v.Check(book.Pages > 0, "pages", "must be more than 0")
-}
